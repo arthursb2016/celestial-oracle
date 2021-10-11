ddbd68b5d8fd14bade13edcd6bf83f8c33220179
Arthur Borba <artsborba@gmail.com>
2021-10-11 17:00:11 -0300

    display some data at _slug.vue

---
+++ components/PageContainer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="page-container"
+    :class="{
+      show,
+    }"
+  >
+    <slot />
+  </div>
+</template>
+<script>
+export default {
+  name: 'PageContainer',
+  components: {},
+  mixins: [],
+  props: {
+    show: {
+      type: Boolean,
+      default: () => false,
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+<style lang="scss" scoped>
+.page-container {
+  height: 100%;
+  width: $page-box-width;
+  background-color: $light-golden;
+  border: 0.25rem solid $harsh-golden;
+  border-bottom: 0px;
+  border-radius: 1rem;
+  padding: 2rem;
+  margin: auto;
+  margin-top: 43rem;
+  opacity: 0.1;
+  transition: margin-top 1000ms ease-out, opacity 300ms ease-in;
+
+  &.show {
+    margin-top: 2rem;
+    opacity: 0.9;
+    box-shadow: -2px 0px 8px $light-golden, 2px 0px 8px $light-golden;
+  }
+}
+</style>
--- pages/angels/_slug.vue
+++ pages/angels/_slug.vue
@@ -1,24 +1,111 @@
 <template>
-  <div class="container">angel page</div>
+  <page-container :show="showPage">
+    <div
+      v-if="angel.slug"
+      class="body"
+    >
+      <div class="image-column">
+        <v-img
+          :src="require(`/assets/angels/${angel.slug}.jpg`)"
+          alt="Image of a beatiful Angel"
+          class="angel-image"
+        />
+        Image from:
+        <a
+          :href="angel.img_author_link"
+          target="_blank"
+        >
+          {{ angel.img_author }}
+        </a>
+      </div>
+      <div class="content-column">
+        <h1 class="angel-name">
+          {{ angel.name }}
+        </h1>
+        <div class="angel-description">
+          {{ angel.description }}
+        </div>
+      </div>
+    </div>
+  </page-container>
 </template>
 <script>
+import PageContainer from '~/components/PageContainer';
+
 export default {
   name: '',
-  components: {},
+  components: {
+    PageContainer,
+  },
   mixins: [],
   props: {},
   data() {
-    return {};
+    return {
+      showPage: false,
+      angel: {},
+    };
   },
   computed: {},
   async asyncData({ params }) {
-    console.log(params)
+    const { slug } = params;
+    return { slug };
+  },
+  async mounted() {
+    /* remove this later */
+    const data = await this.$content('angels')
+      .fetch()
+      .catch((err) => {
+        console.error(err);
+      });
+    this.$store.commit('angels/setAngels', data);
+    /* end of remove later */
+    this.showPage = true;
+    const angel = this.$store.getters['angels/getAngel'](this.slug);
+    if (!angel) {
+      console.error('Angel not found');
+      this.$router.push('/');
+      return;
+    }
+    this.angel = { ...angel };
   },
   methods: {},
 };
 </script>
 <style lang="scss" scoped>
-.container {
-  width: $page-box-width;
+.body {
+  width: 100%;
+  height: 100%;
+  font-size: 1.75rem;
+  color: white;
+  font-family: FANTASY;
+  letter-spacing: 0.1rem;
+  padding-bottom: 1rem;
+  display: flex;
+
+  .image-column {
+    display: flex;
+    flex-direction: column;
+
+    .angel-image {
+      max-width: 20rem;
+      margin-bottom: 2rem;
+    }
+  }
+
+  .content-column {
+    flex-grow: 1;
+    padding-left: 2.5rem;
+    text-align: center;
+
+    .angel-name {
+      border-bottom: 2px solid white;
+      margin-bottom: 1rem;
+    }
+
+    .angel-description {
+      text-align: justify;
+      font-size: 2rem;
+    }
+  }
 }
 </style>
