d3982a4701df541f9d8a4b1e1fa05bb0a352ce4c
Arthur Borba <artsborba@gmail.com>
2021-10-13 18:32:54 -0300

    about page

---
+++ pages/about.vue
@@ -0,0 +1,39 @@
+<template>
+  <page-container>
+    <div class="body">
+      <h2>
+        The Angels
+      </h2>
+      <div class="content">
+        Blah blah blah
+      </div>
+      <h2>
+        About the website
+      </h2>
+      <div class="content">
+        Di di di di
+      </div>
+    </div>
+  </page-container>
+</template>
+<script>
+import PageContainer from '~/components/PageContainer';
+
+export default {
+  name: '',
+  components: {
+    PageContainer,
+  },
+  mixins: [],
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
